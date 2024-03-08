0ca1401a088baf6aaa9310157ff0cf36cfe16400
Kyaw Zin Htet <kyawzinhtet@Kyaws-MacBook-Pro.local>
2024-03-09 00:33:45 +0630

    bug fix

--- app/Components/Temperature/Temperature.tsx
+++ app/Components/Temperature/Temperature.tsx
@@ -1,33 +1,27 @@
 "use client";
-
 import { useGlobalContext } from '@/app/context/globalContext';
 import { drizzleIcon, rain, snow, clearSky, cloudy, navigation } from '@/app/utils/Icons';
 import { kelvinToCelsius } from '@/app/utils/misc';
 import moment from 'moment';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 const Temperature = () => {
+    const { forecast } = useGlobalContext();
 
-    const { forecast} = useGlobalContext();
-
-    const {main, timezone, name, weather} = forecast;
-
-    if(!forecast || !weather) {
-        return  <div>Loading...</div>
+    if (!forecast || !forecast.weather) {
+        return <div>Loading...</div>;
     }
+
+    const { main, timezone, name, weather } = forecast;
 
     const temp = kelvinToCelsius(main?.temp);
     const minTemp = kelvinToCelsius(main?.temp_min);
     const maxTemp = kelvinToCelsius(main?.temp_max);
 
-    //state
-    const [localTime, setLocalTime] = useState<string>("");
-    const [currentDay, setCurrentDay] = useState<string>("");
+    const { main: weatherMain, description } = weather[0];
 
-    const {main: weatherMain, description} = weather[0];
-
-    const getIcon = () =>{
-        switch (weatherMain){
+    const getIcon = () => {
+        switch (weatherMain) {
             case "Drizzle":
                 return drizzleIcon;
             case "Rain":
@@ -43,50 +37,45 @@ const Temperature = () => {
         }
     };
 
-    //live time data
+    const [localTime, setLocalTime] = useState<string>("");
+    const [currentDay, setCurrentDay] = useState<string>("");
+
     useEffect(() => {
-        // upadte time every second
         const interval = setInterval(() => {
-          const localMoment = moment().utcOffset(timezone / 60);
-          // custom format: 24 hour format
-          const formatedTime = localMoment.format("HH:mm:ss");
-          // day of the week
-          const day = localMoment.format("dddd");
+            const localMoment = moment().utcOffset(timezone / 60);
+            const formatedTime = localMoment.format("HH:mm:ss");
+            const day = localMoment.format("dddd");
     
-          setLocalTime(formatedTime);
-          setCurrentDay(day);
+            setLocalTime(formatedTime);
+            setCurrentDay(day);
         }, 1000);
     
-        // clear interval
         return () => clearInterval(interval);
-      }, [timezone]);
+    }, [timezone]);
 
-  return (
-    <div className="pt-6 pb-5 px-4 border rounded-lg flex flex-col 
-        justify-between dark:bg-dark-grey shadow-sm dark:shadow-none"
-    >
-        <p className="flex justify-between items-center">
-            <span className="font-medium">{currentDay}</span>
-            <span className="font-medium">{localTime}</span>
-        </p>
-        <p className="pt-2 font-bold flex gap-1">
-            <span>{name}</span>
-            <span>{navigation}</span>
-        </p>
-        <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
-
-        <div>
-            <div>
-                <span>{getIcon()}</span>
-                <p className="pt-2 capitalize text-lg font-medium">{description}</p>
-            </div>
-                <p className="flex items-center gap-2">
-                <span>Low: {minTemp}°</span>
-                <span>High: {maxTemp}°</span>
+    return (
+        <div className="pt-6 pb-5 px-4 border rounded-lg flex flex-col justify-between dark:bg-dark-grey shadow-sm dark:shadow-none">
+            <p className="flex justify-between items-center">
+                <span className="font-medium">{currentDay}</span>
+                <span className="font-medium">{localTime}</span>
             </p>
+            <p className="pt-2 font-bold flex gap-1">
+                <span>{name}</span>
+                <span>{navigation}</span>
+            </p>
+            <p className="py-10 text-9xl font-bold self-center">{temp}°</p>
+            <div>
+                <div>
+                    <span>{getIcon()}</span>
+                    <p className="pt-2 capitalize text-lg font-medium">{description}</p>
+                </div>
+                <p className="flex items-center gap-2">
+                    <span>Low: {minTemp}°</span>
+                    <span>High: {maxTemp}°</span>
+                </p>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Temperature
+export default Temperature;
